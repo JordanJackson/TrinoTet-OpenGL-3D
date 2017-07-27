cf2b279ff2aa32785f44cd540a28fe2c01433a4a
Jordan Jackson <jordanpatrickjackson@gmail.com>
2017-07-27 08:02:38 -0400

    Added Assimp library to project. Starting model loading.
    Added Assimp library to project. Starting model loading to be handled in
    ResourceManager.

---
+++ TrinoTet OpenGL 3D/src/Model.h
@@ -0,0 +1,10 @@
+#ifndef _SRC_MODEL_H_
+#define _SRC_MODEL_H_
+#pragma once
+
+class Model
+{
+
+};
+
+#endif
--- TrinoTet OpenGL 3D/src/ResourceManager.h
+++ TrinoTet OpenGL 3D/src/ResourceManager.h
@@ -7,6 +7,10 @@
 
 #include <GL/glew.h>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include "Texture.h"
 #include "Shader.h"
 
